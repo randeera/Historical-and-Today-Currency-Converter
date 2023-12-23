809536a1440a4b01d7e8030aaafc085a25303bbe
Kaveendra Randeera <khkrandeera@gmail.com>
2023-12-23 18:45:40 +0530

    Clinet side - front end forms added

--- client/src/components/MainPage.tsx
+++ client/src/components/MainPage.tsx
@@ -39,6 +39,34 @@ export function MainPage() {
                                    placeholder="Select source currency"
                                    required/>
                         </div>
+
+                        <div className="mb-5 mb-4 ">
+                            <label htmlFor=""
+                                   className="block mb-2 text-sm font-medium text-green-500
+                                    dark:text-white">Target Currency</label>
+                            <input type="text" id=""
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500
+                                    focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-grey
+                                     dark:focus:ring-green-500 dark:focus:border-green-500"
+                                   placeholder="Select target currency"
+                                   required/>
+                        </div>
+
+                        <div className="mb-5 mb-4 ">
+                            <label htmlFor=""
+                                   className="block mb-2 text-sm font-medium text-green-500
+                                    dark:text-white">Amount in Currency</label>
+                            <input type="text" id=""
+                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500
+                                    focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-grey
+                                     dark:focus:ring-green-500 dark:focus:border-green-500"
+                                   placeholder="Amount in source currency"
+                                   required/>
+                        </div>
+                        <button type="submit"
+                                className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                            Get the target currency
+                        </button>
                     </form>
                 </section>
             </div>
